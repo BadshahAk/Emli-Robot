98be33dc3f9518d9f8b891c3a867458a6879d16b
Jack Sparrow <91930171+BadshahAk@users.noreply.github.com>
2022-02-12 06:27:17 +0530

    Update __main__.py

--- Emli/__main__.py
+++ Emli/__main__.py
@@ -359,15 +359,15 @@ def emli_about_callback(update, context):
     query = update.callback_query
     if query.data == "emli_":
         query.message.edit_text(
-            text="๏ I'm *Emli*, a powerful group management bot built to help you manage your group easily."
+            text="๏ I'm *Ansi*, a powerful group management bot built to help you manage your group easily."
             "\n• I can restrict users."
             "\n• I can greet users with customizable welcome messages and even set a group's rules."
             "\n• I have an advanced anti-flood system."
             "\n• I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc."
             "\n• I have a note keeping system, blacklists, and even predetermined replies on certain keywords."
             "\n• I check for admins' permissions before executing any command and more stuffs"
-            "\n\n_Emli's licensed under the GNU General Public License v3.0_"
-            "\n\n Click on button bellow to get basic help for Emli.",
+            "\n\n_Ansi's licensed under the GNU General Public License v3.0_"
+            "\n\n Click on button bellow to get basic help for Ansi.",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=False,
             reply_markup=InlineKeyboardMarkup(
